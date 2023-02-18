5645a0b992f3be9995a5911e7680a00bb33685c8
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2023-02-17 18:31:24 -0600

    added hardwareController module and first version of motors

--- src/RLP_TMR2023/main.py
+++ src/RLP_TMR2023/main.py
@@ -1,22 +1,63 @@
-import logging as log
-
-import numpy as np
+from RLP_TMR2023.HardwareControllers.ProbandoSingleton import prueb_motor_falso_factory
 
 
 def sum(a: int, b: int) -> int:
     return a + b
 
 
-def main():
-    log.basicConfig(level=log.DEBUG, format='%(levelname)s - %(message)s')
+def tst_singletons() -> None:
+    # p = PruebaMotorFalso()
+    # p.setup(5)
+    #
+    # p.increment()
+    # p.increment()
+    # # print(p.get_number_of_turns())
+    #
+    # p2 = PruebaMotorFalso()
+    # p2.increment()
+    # # print(p2.get_number_of_turns())
+    #
+    # p3 = PruebaMotorFalso()
+    # # print(p3.get_number_of_turns())
+    pass
 
-    log.debug("debug")
-    log.info("info")
-    log.warning("warning")
-    log.error("error")
-    log.critical("critical")
-    print('Hello World')
-    log.info(np.zeros((3, 3)))
+
+def probando_factory_con_singleton() -> None:
+    # p = prueb_motor_falso_factory(platform.machine())
+    # p.setup(5)
+    # p.increment()
+    # p.increment()
+    #
+    # p.get_number_of_turns()
+    #
+    # p2 = prueb_motor_falso_factory(platform.machine())
+    # p2.increment()
+    # p2.get_number_of_turns()
+    p = prueb_motor_falso_factory("armv7l")
+    p.setup(5)
+    p.increment()
+    p.increment()
+
+    print(p.get_number_of_turns())
+
+    p2 = prueb_motor_falso_factory("armv7l")
+    p2.increment()
+    print(p2.get_number_of_turns())
+
+
+def main():
+    # log.basicConfig(level=log.DEBUG, format='%(levelname)s - %(message)s')
+    #
+    # log.debug("debug")
+    # log.info("info")
+    # log.warning("warning")
+    # log.error("error")
+    # log.critical("critical")
+    # print('Hello World')
+    # log.info(np.zeros((3, 3)))
+
+    # test_singletons()
+    probando_factory_con_singleton()
 
 
 if __name__ == '__main__':
