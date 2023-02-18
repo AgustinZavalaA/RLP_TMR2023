649c27e3306d27ce071488cd0a6df2c93882680f
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2023-02-17 21:21:20 -0600

    added tests and finished refactor of motorscontroller

---
+++ tests/test_hardware_controllers.py
@@ -0,0 +1,37 @@
+import platform
+import unittest
+
+from RLP_TMR2023.hardware_controllers.motors_controller import motors_controller_factory
+
+
+class TestHardwareController(unittest.TestCase):
+    def test_singleton_functionality(self):
+        # TODO: add test for other controllers
+        if platform.machine() not in ['x86_64', 'armv7l']:
+            self.skipTest("This test is only for x86_64 and armv7l")
+
+        m1 = motors_controller_factory(platform.machine())
+        m2 = motors_controller_factory(platform.machine())
+
+        self.assertEqual(m1, m2)
+        self.assertEqual(id(m1), id(m2))
+
+    def test_motors_controller_mock(self):
+        if platform.machine() not in ['x86_64', 'armv7l']:
+            self.skipTest("This test is only for x86_64 and armv7l")
+
+        m = motors_controller_factory("x86_64")
+        self.assertEqual(m.__class__.__name__, 'MotorsControllerMock')
+
+    def test_motors_controller_raspberry(self):
+        if platform.machine() not in ['x86_64', 'armv7l']:
+            self.skipTest("This test is only for x86_64 and armv7l")
+
+        m = motors_controller_factory("armv7l")
+        self.assertEqual(m.__class__.__name__, 'MotorsControllerRaspberry')
+
+    # TODO: add test for other controllers
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_main.py
+++
@@ -1,7 +0,0 @@
-# Remove this code only for testing purposes
-
-from RLP_TMR2023.main import sum
-
-
-def test_sum():
-    assert sum(1, 2) == 3
