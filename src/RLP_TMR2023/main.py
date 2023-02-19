0765e6cbb1090c930ca140ee09765c8c1ef767f4
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2023-02-18 20:23:46 -0600

    added the initial code for detection module

--- src/RLP_TMR2023/main.py
+++ src/RLP_TMR2023/main.py
@@ -1,3 +1,10 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="%(module)s:%(funcName)s - %(levelname)s - %(message)s")
+
+from hardware_controllers import motors_controller
+
+
 def main():
     # log.basicConfig(level=log.DEBUG, format='%(levelname)s - %(message)s')
     #
@@ -8,6 +15,7 @@ def main():
     # log.critical("critical")
     # print('Hello World')
     # log.info(np.zeros((3, 3)))
+    motors_controller.main()
     pass
 
 
