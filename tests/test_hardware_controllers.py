048ae4292fe1ce9d029b5619b1359fbb8526c23b
AgustinZavalaA <1930120@upv.edu.mx>
2023-02-23 21:27:12 -0600

    fixed tests

--- tests/test_hardware_controllers.py
+++ tests/test_hardware_controllers.py
@@ -7,8 +7,8 @@ from RLP_TMR2023.hardware_controllers.motors_controller import motors_controller
 class TestHardwareController(unittest.TestCase):
     def test_singleton_functionality(self):
         # TODO: add test for other controllers
-        if platform.machine() not in ['x86_64', 'armv7l']:
-            self.skipTest("This test is only for x86_64 and armv7l")
+        if platform.machine() not in ['x86_64', 'aarch64']:
+            self.skipTest("This test is only for x86_64 and aarch64")
 
         m1 = motors_controller_factory(platform.machine())
         m2 = motors_controller_factory(platform.machine())
@@ -17,17 +17,17 @@ class TestHardwareController(unittest.TestCase):
         self.assertEqual(id(m1), id(m2))
 
     def test_motors_controller_mock(self):
-        if platform.machine() not in ['x86_64', 'armv7l']:
-            self.skipTest("This test is only for x86_64 and armv7l")
+        if platform.machine() not in ['x86_64', 'aarch64']:
+            self.skipTest("This test is only for x86_64 and aarch64")
 
         m = motors_controller_factory("x86_64")
         self.assertEqual(m.__class__.__name__, 'MotorsControllerMock')
 
     def test_motors_controller_raspberry(self):
-        if platform.machine() not in ['x86_64', 'armv7l']:
-            self.skipTest("This test is only for x86_64 and armv7l")
+        if platform.machine() not in ['x86_64', 'aarch64']:
+            self.skipTest("This test is only for x86_64 and aarch64")
 
-        m = motors_controller_factory("armv7l")
+        m = motors_controller_factory("aarch64")
         self.assertEqual(m.__class__.__name__, 'MotorsControllerRaspberry')
 
     # TODO: add test for other controllers
