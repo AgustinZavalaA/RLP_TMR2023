5858f51395cb97f798c786fcaf3ad7f8a47246d0
Jesús Quiñones <1930318@upv.edu.mx>
2023-03-18 16:03:08 -0600

    Uploading some reading data test from mpu

--- src/RLP_TMR2023/mpu_tests/mpu_test.py
+++ src/RLP_TMR2023/mpu_tests/mpu_test.py
@@ -1,43 +1,102 @@
 # Delete when we don't need it anymore
-
+import logging
 import time
+from abc import abstractmethod
+import platform
+from typing import Type, Mapping
 
-import numpy as np
 from mpu9250_jmdev.mpu_9250 import MPU9250
 from mpu9250_jmdev.registers import \
-    AK8963_ADDRESS, MPU9050_ADDRESS_68, GFS_1000, AFS_8G, AK8963_BIT_16, AK8963_MODE_C100HZ
+    MPU9050_ADDRESS_68, GFS_1000, AFS_8G, AK8963_BIT_16, AK8963_MODE_C100HZ
 
-mpu = MPU9250(
-    address_ak=0x68,
-    address_mpu_master=MPU9050_ADDRESS_68,  # In 0x68 Address
-    address_mpu_slave=None,
-    bus=1,
-    gfs=GFS_1000,
-    afs=AFS_8G,
-    mfs=AK8963_BIT_16,
-    mode=AK8963_MODE_C100HZ)
+from RLP_TMR2023.hardware_controllers.singleton import Singleton
 
-time.sleep(1)
-mpu.configure()
+logger = logging.getLogger(__name__)
 
-var = 100
-data_accel = np.zeros([var, 3])
-data_mag = np.zeros([var, 3])
-data_gyro = np.zeros([var, 3])
 
-for i in range(var):
-    accel = mpu.readAccelerometerMaster()
-    mag = mpu.readMagnetometerMaster()
-    gyro = mpu.readGyroscopeMaster()
-    print(f" {i}"
-          f"Accelerometer:      {accel}\n"
-          f"Magnetometer:       {mag}\n"
-          f"Gyroscope:          {gyro}")
-    data_accel[i] = accel
-    data_mag[i] = mag
-    data_gyro[i] = gyro
+class IMUController(metaclass=Singleton):
+    @abstractmethod
+    def setup(self) -> None:
+        pass
 
-# save a numpy array in csv file
-np.save("data_accel.csv", data_accel)
-np.save("data_mag.csv", data_mag)
-np.save("data_gyro.csv", data_gyro)
+    @abstractmethod
+    def is_robot_stuck(self) -> bool:
+        pass
+
+    def disable(self) -> None:
+        pass
+
+
+class IMUControllerMock(IMUController):
+    def __init__(self):
+        super().__init__()
+        logger.info("Instantiating Singleton IMUControllerMock")
+
+    def setup(self) -> None:
+        logger.info("IMUControllerMock.setup() called")
+
+    def is_robot_stuck(self) -> bool:
+        logger.info("IMUControllerMock.is_robot_stuck() called")
+        return False
+
+    def disable(self) -> None:
+        logger.info("IMUControllerMock.disable() called")
+
+
+class IMUControllerRaspberry(IMUController):
+    def __init__(self):
+        super().__init__()
+        logger.info("Instantiating Singleton IMUControllerRaspberry")
+        self.mpu = MPU9250(
+            address_ak=0x68,
+            address_mpu_master=MPU9050_ADDRESS_68,
+            address_mpu_slave=None,
+            bus=1,
+            gfs=GFS_1000,
+            afs=AFS_8G,
+            mfs=AK8963_BIT_16,
+            mode=AK8963_MODE_C100HZ
+        )
+
+    def setup(self) -> None:
+        logger.info("IMUControllerRaspberry.setup() called")
+        self.mpu.configure()
+
+    def is_robot_stuck(self) -> None:
+        logger.info("IMUControllerRaspberry.is_robot_stuck() called")
+        # create a function that returns true if the robot is stuck
+        gyro = self.mpu.readGyroscopeMaster()
+        print(gyro)
+        accel = self.mpu.readAccelerometerMaster()
+        print(accel)
+
+    def disable(self) -> None:
+        logger.info("IMUControllerRaspberry.disable() called")
+
+
+def imu_controller_factory(architecture: str) -> IMUController:
+    """
+    This function is used to return the correct IMUController class depending on the platform
+    :return: the correct IMUController class
+    """
+    constructors: Mapping[str, Type[IMUController]] = {
+        "x86_64": IMUControllerMock,
+        "AMD64": IMUControllerMock,
+        # "aarch64": IMUControllerMockRaspberry, #TODO: implement this class
+    }
+    return constructors[architecture]()
+
+
+while True:
+    logging.basicConfig(level=logging.DEBUG)
+    imu_controller = imu_controller_factory(platform.machine())
+    imu_controller.setup()
+    imu_controller.is_robot_stuck()
+    # print("|.....MPU9250 in 0x68 Address.....|")
+    # print("Accelerometer", mpu.readAccelerometerMaster())
+    # print("Gyroscope", mpu.readGyroscopeMaster())
+    # print("Magnetometer", mpu.readMagnetometerMaster())
+    # print("Temperature", mpu.readTemperatureMaster())
+    # print("\n")
+
+    time.sleep(1)
