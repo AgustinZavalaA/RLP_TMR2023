87ac95503914fdc8dc420e7a7b0476b9ac4856be
Jesús Quiñones <1930318@upv.edu.mx>
2023-03-26 16:23:41 -0600

    changes for image display un oled screen

--- src/RLP_TMR2023/oled_screen/oled_screen_stats.py
+++ src/RLP_TMR2023/oled_screen/oled_screen_stats.py
@@ -74,12 +74,17 @@ while True:
 
     # Write four lines of text.
 
-    draw.text((x, top + 0), "IP: " + IP, fill=255)
-    draw.text((x, top + 8), "CPU load: " + CPU, font=font, fill=255)
-    draw.text((x, top + 16), MemUsage, font=font, fill=255)
-    draw.text((x, top + 25), Disk, font=font, fill=255)
+    # draw.text((x, top + 0), "IP: " + IP, fill=255)
+    
+    # draw.text((x, top + 8), "CPU load: " + CPU, font=font, fill=255)
+    # draw.text((x, top + 16), MemUsage, font=font, fill=255)
+    # draw.text((x, top + 25), Disk, font=font, fill=255)
 
     # Display image.
-    disp.image(image)
+    # disp.image(image)
+    disp.text('IP: ' + IP, 0, 0, 1)
+    disp.text('CPU load: ' + CPU, 0, 8, 1)
+    disp.text(MemUsage, 0, 16, 1)
+    disp.text(Disk, 0, 25, 1)
     disp.show()
     time.sleep(0.1)
