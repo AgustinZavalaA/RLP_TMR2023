b398bf1fe62c53cc69fdee1f048714f49d5e10d7
Jesús Quiñones <1930318@upv.edu.mx>
2023-03-26 16:54:56 -0600

    Changes for oled_stats

--- src/RLP_TMR2023/oled_screen/oled_screen_stats.py
+++ src/RLP_TMR2023/oled_screen/oled_screen_stats.py
@@ -1,11 +1,3 @@
-# SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
-# SPDX-FileCopyrightText: 2017 James DeVito for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# This example is for use on (Linux) computers that are using CPython with
-# Adafruit Blinka to support CircuitPython libraries. CircuitPython does
-# not support PIL/pillow (python imaging library)!
-
 import time
 import subprocess
 
@@ -13,7 +5,6 @@ from board import SCL, SDA
 import busio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
-
 
 # Create the I2C interface.
 i2c = busio.I2C(SCL, SDA)
@@ -27,42 +18,7 @@ disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 disp.fill(0)
 disp.show()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new("1", (width, height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-bottom = height - padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
-
-
-# Load default font.
-font = ImageFont.load_default()
-
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the
-# same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
-
 while True:
-
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-    # Shell scripts for system monitoring from here:
-    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d' ' -f1"
     IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
     cmd = 'cut -f 1 -d " " /proc/loadavg'
@@ -72,18 +28,9 @@ while True:
     cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
     Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
-    # Write four lines of text.
-
-    # draw.text((x, top + 0), "IP: " + IP, fill=255)
-    
-    # draw.text((x, top + 8), "CPU load: " + CPU, font=font, fill=255)
-    # draw.text((x, top + 16), MemUsage, font=font, fill=255)
-    # draw.text((x, top + 25), Disk, font=font, fill=255)
-
     # Display image.
     disp.fill(0)
     new_font = "src/RLP_TMR2023/oled_screen/font5x8.bin"
-    # disp.image(image)
     disp.text('IP: ' + IP, 0, 0, 1, font_name=new_font)
     disp.text('CPU load: ' + CPU, 0, 8, 1, font_name=new_font)
     disp.text(MemUsage, 0, 16, 1, font_name=new_font)
