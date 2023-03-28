d10954539ad4fe15a1337b88af9c63579c3d7ef8
Alfredo623 <alfredoguevara2309@gmail.com>
2023-03-27 19:26:52 -0600

    Driver para sensores de distancia.

--- src/RLP_TMR2023/hardware_controllers/distance_sensors_controller.py
+++ src/RLP_TMR2023/hardware_controllers/distance_sensors_controller.py
@@ -6,48 +6,48 @@ import platform
 import time
 from abc import abstractmethod
 from typing import Type, Mapping
-
+import smbus
 from RLP_TMR2023.hardware_controllers.singleton import Singleton
-
 logger = logging.getLogger(__name__)
-
-
 class DistanceSensorsController(metaclass=Singleton):
     @abstractmethod
     def setup(self) -> None:
         pass
-
     @abstractmethod
     def is_about_to_collide(self) -> bool:
         pass
-
     @abstractmethod
     def disable(self) -> None:
         pass
-
-
 class DistanceSensorsControllerMock(DistanceSensorsController):
     """
     This class is a mock for the DistanceSensorsController class, it is used when the program is running on a computer
     """
-
     def __init__(self):
         super().__init__()
         logger.info("Instantiating Singleton DistanceSensorsControllerMock")
-
     def setup(self) -> None:
         logger.info("DistanceSensorsControllerMock.setup() called")
-
     def is_about_to_collide(self) -> bool:
         logger.info("Sensing distance")
         return False
-
     def disable(self) -> None:
         logger.info("DistanceSensorsControllerMock.disable() called")
+class DistanceSensorsControllerRaspberry(DistanceSensorsController):
+    def __init__(self):
+        super().__init__()
+    def setup(self) -> None:
+        self.addr = 8
+        self.i2c_bus= smbus.SMBus(self.addr)
+    def is_about_to_collide(self) -> bool:
+        dist = smbus.read_byte(self.addr, force=None)
+        sens1, sens2, sens3 = dist.split(' ', 2)
+        return sens1 > 20 and sens2 > 20 and sens3 > 20
+
+    def disable(self) -> None:
+        pass
 
 
-# TODO implement this class
-# class DistanceSensorsControllerRaspberry(DistanceSensorsController):
 
 def distance_sensors_controller_factory(architecture: str) -> DistanceSensorsController:
     """
@@ -57,11 +57,9 @@ def distance_sensors_controller_factory(architecture: str) -> DistanceSensorsCon
     constructors: Mapping[str, Type[DistanceSensorsController]] = {
         "x86_64": DistanceSensorsControllerMock,
         "AMD64": DistanceSensorsControllerMock,
-        # "aarch64": DistanceSensorsControllerMockRaspberry, #TODO: implement this class
+        "aarch64": DistanceSensorsControllerRaspberry,
     }
     return constructors[architecture]()
-
-
 def main():
     logging.basicConfig(level=logging.INFO)
     distance_sensors = distance_sensors_controller_factory(platform.machine())
@@ -73,7 +71,5 @@ def main():
     except KeyboardInterrupt:
         distance_sensors.disable()
         logger.info("Exiting program")
-
-
 if __name__ == '__main__':
     main()
