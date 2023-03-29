ad545617dec11c6bf0d4c5d94b345fe0e6606cf8
Alfredo623 <alfredoguevara2309@gmail.com>
2023-03-28 18:28:55 -0600

    Base para driver de servomotores

--- src/RLP_TMR2023/hardware_controllers/servos_controller.py
+++ src/RLP_TMR2023/hardware_controllers/servos_controller.py
@@ -4,15 +4,20 @@ import platform
 from abc import abstractmethod
 from typing import Type, Mapping
 
+from board import SCL, SDA
+import busio
+
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+
 from RLP_TMR2023.hardware_controllers.singleton import Singleton
 
 logger = logging.getLogger(__name__)
 
-
 class ServoPair(enum.Enum):
-    ARM = (0, 1)
-    CLAW = (2, 3)
-    TRAY = (4, 5)
+    ARM = (1, 2)
+    CLAW = (3, 4)
+    TRAY = (5, 6)
 
 
 class ServoStatus(enum.Enum):
@@ -102,10 +107,63 @@ class ServosControllerMock(ServosController):
         logger.info("ServosControllerMock.disable() called")
 
 
-# TODO: implement this class
-# class ServosControllerRaspberry(ServosController):
-# TODO: you remember in setup you have to verify that the servos are in the correct position
-# also you can add any other method you need like move in the arduino repository
+class ServosControllerRaspberry(ServosController):
+    def __init__(self):
+        super().__init__()
+        self._servos_status = {
+            ServoPair.ARM: ServoStatus.RETRACTED,
+            ServoPair.CLAW: ServoStatus.RETRACTED,
+            ServoPair.TRAY: ServoStatus.RETRACTED,
+        }
+        self._servos_values = {
+            ServoPair.ARM: {
+                ServoStatus.EXPANDED: 90,  # TODO: obtain this value from a config file
+                ServoStatus.RETRACTED: 0,
+
+            },
+            ServoPair.CLAW: {
+                ServoStatus.EXPANDED: 30,
+                ServoStatus.RETRACTED: 0,
+            },
+            ServoPair.TRAY: {
+                ServoStatus.EXPANDED: 60,
+                ServoStatus.RETRACTED: 0,
+            },
+        }
+
+    def setup(self) -> None:
+        self.i2c = busio.I2C(SCL, SDA)
+        self.pca = PCA9685(self.i2c)
+        self.pca.frequency = 50
+
+        servo.Servo(self.pca.channels[ServoPair.values])
+
+
+        # verify that the servos are in the correct position
+        for servo_pair, _ in self._servos_status.items():
+            self.move(servo_pair, ServoStatus.RETRACTED, bypass_check=True)
+
+
+    def toggle(self, servo_pair: ServoPair) -> None:
+        s1, s2 = servo.Servo(self.pca.channels[ServoPair.values])
+        if self._servos_status[servo_pair] == ServoStatus.RETRACTED:
+            s1.angle, s2.angle = self._servos_values[servo_pair][self._servos_status[servo_pair]]
+            self._servos_status[servo_pair] = ServoStatus.EXPANDED
+        else:
+            s1.angle, s2.angle = self._servos_values[servo_pair][self._servos_status[servo_pair]]
+            self._servos_status[servo_pair] = ServoStatus.RETRACTED
+
+    def move(self, servo_pair: ServoPair, status: ServoStatus, bypass_check: bool = False) -> None:
+        s1, s2 = servo.Servo(self.pca.channels[ServoPair.values])
+        if not bypass_check and self._servos_status[servo_pair] == status:
+            s1.angle, s2.angle = self._servos_values[servo_pair][self._servos_status[servo_pair]]
+            return
+        self._servos_status[servo_pair] = status
+
+
+    def disable(self) -> None:
+        pass
+
 def servos_controller_factory(architecture: str) -> ServosController:
     """
     This function is used to return the correct ServosController class depending on the platform
@@ -114,7 +172,7 @@ def servos_controller_factory(architecture: str) -> ServosController:
     constructors: Mapping[str, Type[ServosController]] = {
         "x86_64": ServosControllerMock,
         "AMD64": ServosControllerMock,
-        # "aarch64": ServosControllerMockRaspberry, #TODO: implement this class
+        "aarch64": ServosControllerRaspberry,
     }
     return constructors[architecture]()
 
@@ -130,9 +188,6 @@ def main():
         servos.toggle(ServoPair.TRAY)
         logger.info("")
 
-    servos.move(ServoPair.ARM, ServoStatus.EXPANDED)
-    servos.move(ServoPair.ARM, ServoStatus.RETRACTED)
-    servos.move(ServoPair.ARM, ServoStatus.RETRACTED)
 
     servos.disable()
 
