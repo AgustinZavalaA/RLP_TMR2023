1fed4b76657cf34493712c899bd92230bc884ab8
Alfredo623 <alfredoguevara2309@gmail.com>
2023-04-07 20:46:21 -0600

    Servomotores[Pruebas]

--- src/RLP_TMR2023/hardware_controllers/servos_controller.py
+++ src/RLP_TMR2023/hardware_controllers/servos_controller.py
@@ -42,11 +42,11 @@ class ServosController(metaclass=Singleton):
                 ServoStatus.RETRACTED: 0,
             },
             ServoPair.CLAW: {
-                ServoStatus.EXPANDED: 30,
+                ServoStatus.EXPANDED: 90,
                 ServoStatus.RETRACTED: 0,
             },
             ServoPair.TRAY: {
-                ServoStatus.EXPANDED: 60,
+                ServoStatus.EXPANDED: 90,
                 ServoStatus.RETRACTED: 0,
             },
         }
