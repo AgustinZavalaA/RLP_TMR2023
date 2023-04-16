3db348cbcbda0a1b621ac9c5a7b79f649f0c6397
RycheMS <130903229+RycheMS@users.noreply.github.com>
2023-04-16 17:00:45 -0600

    Update buzzer_controller.py

--- src/RLP_TMR2023/hardware_controllers/buzzer_controller.py
+++ src/RLP_TMR2023/hardware_controllers/buzzer_controller.py
@@ -108,7 +108,7 @@ class BuzzerController(metaclass=Singleton):
                 Note(20,0.1,1000),
                 Note(0,0.1),
                 Note(20,0.1,1000),
-                Note(0,0.2),
+                Note(0,0.3),
                 Note(20,0.1,1000),
                 Note(0,0.7),
                 Note(20,0.1,1000),
