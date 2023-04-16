711a95ed22fd6519cf4649af55b0ad655c48f3eb
RycheMS <130903229+RycheMS@users.noreply.github.com>
2023-04-16 15:34:16 -0600

    Update buzzer_controller.py

--- src/RLP_TMR2023/hardware_controllers/buzzer_controller.py
+++ src/RLP_TMR2023/hardware_controllers/buzzer_controller.py
@@ -40,11 +40,10 @@ class BuzzerController(metaclass=Singleton):
         super().__init__()
         self._melodies: dict[Melody, list[Note]] = {
             Melody.CAN_FOUND: [
-                Note(30, 0.1),
-                Note(0, 0.1),
-                Note(70, 0.1),
-                Note(80, 0.1),
-                Note(90, 0.3),
+                Note(70, 0.01),
+                Note(80, 0.01),
+                Note(90, 0.01),
+                Note(99, 0.3),
             ],
             Melody.ABOUT_TO_COLLIDE: [
                 Note(90, 1),
