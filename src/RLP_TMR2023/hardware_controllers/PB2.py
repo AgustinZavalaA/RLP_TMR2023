887a47fb241ba5649220cca700d452e3a0235a74
RycheMS <130903229+RycheMS@users.noreply.github.com>
2023-04-16 05:57:23 -0600

    Update PB2.py

--- src/RLP_TMR2023/hardware_controllers/PB2.py
+++ src/RLP_TMR2023/hardware_controllers/PB2.py
@@ -7,8 +7,9 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(triggerPIN,GPIO.OUT)
 
 buzzer = GPIO.PWM(triggerPIN, 2000) # Set frequency to 1 Khz
+buzzer.start(0) # Set dutycycle to 10
 for i in range (0,100,10):
-    
+
     buzzer.ChangeDutyCycle(10)
     time.sleep(1)
 
