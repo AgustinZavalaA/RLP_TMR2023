cd125c5d9fa371b06fbcef3d0a6eaa20a028f49a
Jesús Quiñones <1930318@upv.edu.mx>
2023-04-25 06:56:55 -0600

    code update, it doesn't work

--- src/RLP_TMR2023/image_processing/image_cropped.py
+++ src/RLP_TMR2023/image_processing/image_cropped.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 import numpy.typing as npt
 
-from RLP_TMR2023.image_processing.image_filter import blue_filter
+from RLP_TMR2023.image_processing.blue_filter import blue_filter
+from RLP_TMR2023.constants.color_filters import BLUE_LOWER_HSV, BLUE_UPPER_HSV
 
 
 def trimmer_image(image: npt.NDArray[np.uint8], percentage_trimmed: int) -> npt.NDArray[np.uint8]:
@@ -16,6 +17,21 @@ def trimmer_image(image: npt.NDArray[np.uint8], percentage_trimmed: int) -> npt.
     return cropped_image  # type: ignore
 
 
+def check_water(image: npt.NDArray[np.uint8]) -> npt.NDArray[np.uint8]:
+    """
+    Check if the water is present in the image
+
+    """
+    new_image = trimmer_image(image, 80)
+    # make binarize render in white and black
+    mask = cv2.inRange(new_image, BLUE_LOWER_HSV, BLUE_UPPER_HSV)
+    output = cv2.bitwise_and(new_image, new_image, mask=mask)
+    # count the number of white pixels
+    ratio_water = cv2.countNonZero(mask) / (image.size / 3)
+    color_percent = (ratio_water * 100) / .3
+    return output, color_percent
+
+
 def main():
     camera = cv2.VideoCapture(0)
     while True:
@@ -24,8 +40,11 @@ def main():
 
         cut_image = trimmer_image(img, 80)
 
+        water_image = check_water(img)
+
         cv2.imshow("Original", img)
         cv2.imshow("Cut and filtered blue", cut_image)
+        cv2.imshow("Water", water_image)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             camera.release()
